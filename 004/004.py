f35344b0d9cd32cfe75c628307d61c8a6b2843ad
Angela Cordova <anvicordova@gmail.com>
2016-06-11 18:56:21 -0500

    004 - Python

---
+++ 004/004.py
@@ -0,0 +1,47 @@
+tenx = 10;
+
+def isPalindromic(number):
+    decomposed_number = []
+    reversed_number = []
+
+    while (number / tenx > 0):
+        pivot = number / tenx
+        pivot *= tenx
+
+        last = number - pivot
+        decomposed_number.append(last)
+        pivot /= tenx
+        number = pivot
+
+    decomposed_number.append(number)
+
+    reversed_number = decomposed_number
+    reversed_number = reversed_number[::-1]
+
+    maxn = len(decomposed_number)
+
+    for i in range(0, maxn):
+        if decomposed_number[i] != reversed_number[i]:
+            return False
+    return True
+
+number = range(100,1000)
+number = number[::-1]
+
+
+
+def start():
+    largest = 0
+    for i in number:
+        update = number
+        for j in update:
+            result = i * j
+            boolean = isPalindromic(result)
+            if boolean:
+                if(result > largest):
+                    largest = result
+        update.pop(update.index(i))
+    return largest
+
+
+print start()
